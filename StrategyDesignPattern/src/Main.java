7a7928e6ea43135d9f9e904638583a47305e9516
Sanskar <sanskarmaliwad1@gmail.com>
2025-02-14 17:04:45 +0530

    Update Main Class

--- StrategyDesignPattern/src/Main.java
+++ StrategyDesignPattern/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-//        Vehicle gv = new GoodsVehicle();
-//        gv.drive();
+        Vehicle gv = new GoodsVehicle();
+        gv.drive();
 
         SportsCar mustang = new SportsCar();
-//        Vehicle nexon = new SUV();
+        Vehicle nexon = new SUV();
 
         mustang.drive();
-//        nexon.drive();
+        nexon.drive();
     }
 }
